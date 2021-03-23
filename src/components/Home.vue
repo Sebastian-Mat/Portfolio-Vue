704d139295e6af4bd3f7ed8454d97083d45317dd
Sebastian-Mat <67850496+Sebastian-Mat@users.noreply.github.com>
2021-03-23 16:01:31 -0500

    Initial Project

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    components: {
+    },
+
+    setup() {
+        
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
