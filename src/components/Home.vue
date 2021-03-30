e0f8424089eeb5de3732bbc24009d4616eccd7f3
Sebastian-Mat <67850496+Sebastian-Mat@users.noreply.github.com>
2021-03-29 21:16:03 -0500

    ADD: Presentation Screen and Nav Component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,179 @@
 <template>
-    
+    <div>
+        <Nav />
+        <div class="presentation">
+
+            <div class="loader">
+                <div class="wrapper">
+                    <div class="box"></div>
+                </div>
+            </div>
+
+            <div class="screen">
+                <div class="img-box">
+                    <div class="cover-img"></div>
+                    <img src="../assets/img/presentation.jpg" alt="">
+                </div>
+                <div class="title">
+                    <h1>M</h1>
+                    <h1>A</h1>
+                    <h1>R</h1>
+                    <h1>K</h1>
+                    <h1>H</h1>
+                    <h1>A</h1>
+                </div>
+                <h1 class="text">PHOTOGRAPHER</h1>
+                <h1 class="text2">001</h1>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+
+import { TimelineLite, Expo, TweenMax } from "gsap";
+import Nav from "./Nav.vue";
+
 export default {
     name: 'Home',
     components: {
+        Nav
     },
 
     setup() {
         
     },
+
+    mounted() {
+        const loader = new TimelineLite();
+
+        loader.from(".box", 1.2, {
+            delay: 1.5,
+            y: 200,
+            height:"0vh",
+            ease: Expo.easeInOut,
+        }).to(".box", 1.2, {
+            y: -200,
+            height: "0vh",
+            ease: Expo.easeInOut,
+        }).to(".loader", 1, {   
+            opacity: 0,
+            ease: Expo.easeInOut,
+        });
+        
+        TweenMax.to(".cover-img", 1.5, {
+            y: "-100%",
+            delay: 3.6,
+            ease: Expo.easeInOut,
+        })
+        TweenMax.staggerFrom(".title h1", 1, {
+            y: 50,
+            opacity: 0,
+            delay: 3.7,
+            ease: Expo.easeInOut,
+        }, 0.1)
+        TweenMax.from(".text, .text2", 1, {
+            y: 50,
+            opacity: 0,
+            delay: 3.9,
+            ease: Expo.easeInOut,
+        })
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.presentation{
+    height: 100vh;
+    width: 100vw;
+    position: relative;
+    overflow: hidden;
+
+    .loader{
+        position: absolute;
+        background-color: black;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 2;
+
+        .wrapper{
+            height: 80%;
+            width: 100%;
+            overflow: hidden;
+            position: relative;
+
+            .box{
+                position: absolute;
+                height: 20vh;
+                width: 5vw;
+                background-color: white;
+                transform: translate(-50%, -50%);
+                top: 50%;
+                left: 50%;
+            }
+        }
+    }
+    
+    .screen{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        height: 100%;
+        width: 100%;
+
+        .img-box{
+            height: 100%;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            overflow: hidden;
+            position: relative;
+
+            .cover-img{
+                background-color: white;
+                width: 100%;
+                height: 100%;
+                position: absolute;
+            }
+
+            img{
+                height: auto;
+                width: 30%;    
+            }
+        }
+
+        .title{
+            position: absolute;
+            font-size: 7vw;
+            display: flex;
+            flex-direction: row;
+            -webkit-text-fill-color: transparent;
+            -webkit-text-stroke-width: 0.15vw;
+        }
+
+        .text{
+            position: absolute;
+            font-size: 1rem;
+            bottom: 40px;
+            left: 80px;
+            text-decoration: underline;
+            font-family: monument
+        }
+
+        .text2{
+            position: absolute;
+            font-size: 3rem;
+            top: 100px;
+            right: 200px;
+            font-family: monument
+        }
+    }
+}
 
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="nav">
+    <div class="left">
+      <a href="">About</a>
+      <a href="">Projects</a>
+    </div>
+    <div class="right">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-message-circle"
+      >
+        <path
+          d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"
+        ></path>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { Expo, TweenMax } from "gsap";
+
+export default {
+  name: "Nav",
+  setup() {},
+  mounted() {
+    TweenMax.staggerFrom(".left a, .right svg", 1, {
+        y: 100,
+        opacity: 0,
+        delay: 3.7,
+        ease: Expo.easeInOut,
+    }, 0.1)
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.nav{
+    padding: 0 80px;
+    height: 100px;
+    width: 100%;
+    background-color: transparent;
+    color: black;
+    font-family: Monument;
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: fixed;
+
+    a{
+        text-decoration: none;
+        color: black;
+        margin-right: 30px;
+    }
+}
+
+</style>
